6329107fffe55894e396f4f8f365f0b5c2896c23
flingyp <1213563369@qq.com>
2023-03-17 22:07:54 +0800

    refactor: remove vue-i18n

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,8 @@
 import { App } from 'vue'
 import { usePinia } from './pinia'
-import { useVueI18n } from './vue-i18n'
 import { useVueRouter } from './vue-router'
 
 export default async (app: App) => {
   useVueRouter(app)
-  useVueI18n(app)
   usePinia(app)
 }
