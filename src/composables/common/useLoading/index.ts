d132185805dc7ccb3e6d4492b36f3171a2f73e64
YePeng <1213563369@qq.com>
2023-09-11 16:04:07 +0800

    refactor: optimize useLoading composable

--- src/composables/common/useLoading/index.ts
+++ src/composables/common/useLoading/index.ts
@@ -1,30 +1,23 @@
-/**
- * Can configure the loading property of the BaseSpin component or the Button component to implement the Loading effect
- * @returns
- */
 export const useLoading = (defaultValue = false) => {
   const isLoading = ref(defaultValue)
 
-  const setLoading = async (value: boolean) => {
+  const setLoading = (value: boolean) => {
     isLoading.value = value
   }
 
-  // eslint-disable-next-line no-unused-vars
-  const withLoading = async <T>(callback: (...args: unknown[]) => Promise<T>) => {
+  const withLoading = async <T>(callback: () => Promise<T>) => {
     if (isLoading.value) return
-
-    isLoading.value = true
-
-    const response = await callback()
-
-    isLoading.value = false
-
-    return response
+    try {
+      setLoading(true)
+      const response = await callback()
+      return response
+    } finally {
+      setLoading(false)
+    }
   }
 
   return {
     isLoading: readonly(isLoading),
-
     setLoading,
     withLoading,
   }
